a50a5e8b1d99c13b5fb1d5879d652cfb51a36d29
Oleksiy <Unialex@ukr.net>
2025-03-18 18:30:34 +0200

    done

--- src/main.js
+++ src/main.js
@@ -13,14 +13,13 @@ const refs = {
 };
 
 refs.form.addEventListener('submit', handleSubmitClick);
-refs.moreImgBtn.addEventListener('click', handleMoreClick);
 let page, per_page, request;
 
 async function handleSubmitClick(event) {
   event.preventDefault();
   refs.request.setAttribute('readonly', true); //readonly for input
-  refs.submitBtn.disabled = true; //disable "Submite" button
-  refs.loader.classList.remove('hidden'); //show loader text
+  refs.submitBtn.disabled = true; // disable "Submite" button
+  refs.loader.classList.remove('hidden'); // show loader
 
   page = 1;
   per_page = 20;
@@ -28,42 +27,45 @@ async function handleSubmitClick(event) {
   const images = await searchImage(request, page, per_page);
 
   refs.request.removeAttribute('readonly'); // Restore elements
-  refs.submitBtn.disabled = false; //  state
-  refs.loader.classList.add('hidden'); // hide loader text
+  refs.submitBtn.disabled = false; //               state
 
+  refs.loader.classList.add('hidden'); // hide loader
   if (!images) {
     return;
   }
+
   refs.gallery.innerHTML = '';
   refs.request.value = '';
   renderGallary(images, refs.gallery);
-  needMoreBtnCheck(images);
+  needMoreBtnCheck(images); //   Show/hide "More" button
 }
 
 async function handleMoreClick(event) {
   refs.loader.classList.remove('hidden'); //show loader text
   const images = await searchImage(request, ++page, per_page);
-
   if (!images) {
     return;
   }
-  const lastGalleryCard = refs.gallery.lastElementChild;
+
+  const lastGalleryCard = refs.gallery.lastElementChild; //remember last element before new will add
   renderGallary(images, refs.gallery);
   refs.loader.classList.add('hidden'); // hide loader text
-  needMoreBtnCheck(images);
+  needMoreBtnCheck(images); //   Show/hide "More" button
 
   const { top: lastCardPos } = lastGalleryCard.getBoundingClientRect();
-  window.scrollBy({ top: lastCardPos - 24, behavior: 'smooth' });
+  window.scrollBy({ top: lastCardPos - 24, behavior: 'smooth' }); //scroll to top border of the card
 }
 
 function needMoreBtnCheck(images) {
   if (images.length < per_page) {
     refs.moreImgBtn.classList.add('hidden'); //hide "More" button
+    refs.moreImgBtn.removeEventListener(handleMoreClick);
     iziToast.info({
       position: 'topRight',
       message: "We're sorry, but you've reached the end of search results.",
     });
   } else {
     refs.moreImgBtn.classList.remove('hidden'); //show "More" button
+    refs.moreImgBtn.addEventListener('click', handleMoreClick);
   }
 }
